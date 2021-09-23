9de190758878f68ebf93c398880dfc839b2b1133
snozaki <snozaki@netprotections.co.jp>
2021-09-23 22:32:04 +0900

    シンボルでdiできるよう修正

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { InMemoryTasksReposiory } from './repository/inmemory.tasks.repository';
 import { TasksController } from './tasks.controller';
 
 @Module({
   controllers: [TasksController],
+  providers: [
+    {
+      provide: 'TasksRepository',
+      useClass: InMemoryTasksReposiory,
+    },
+  ],
 })
 export class TasksModule {}
